b095d4ab3a82e90585352813f21926fc1c298cfc
Giang <jun154607@gmail.com>
2021-05-06 09:48:41 +0700

    edit route/api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+if (App::environment('production', 'staging')) {
+    URL::forceScheme('https');
+}
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
